e2b8f0a2cec627a19919d8af77bf8de741781017
Ashwin <ashwin.mallik@mail.utoronto.ca>
2026-01-20 12:43:56 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (15.18%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,19 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        red = 0
+        white = 0
+        blue = len(nums) - 1
         
-        freq = {0:0, 1:0, 2:0}
-
-        for num in nums:
-            freq[num] += 1
-        
-        for i in range(len(nums)):
-            if freq[0] != 0:
-                nums[i] = 0
-                freq[0] -= 1
-            elif freq[1] != 0:
-                nums[i] = 1
-                freq[1] -= 1
+        while white <= blue:
+            if nums[white] == 0:
+                nums[white], nums[red] = nums[red], nums[white]
+                red += 1
+                white += 1
+            elif nums[white] == 1:
+                white += 1
             else:
-                nums[i] = 2
-                freq[2] -= 1      
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
