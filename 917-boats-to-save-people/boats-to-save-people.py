9f0c5e128311d70a645c9366c32e066621eb8365
Ashwin <ashwin.mallik@mail.utoronto.ca>
2026-08-13 22:19:44 -0400

    Time: 47 ms (88.88%) | Memory: 25.3 MB (17.35%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -1,19 +1,25 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        
         ans = 0
         left = 0
         right = len(people) - 1
         people.sort()
 
         while left <= right:
+            if left == right:
+                ans += 1
+                break
+
             weight = people[right] + people[left]
+
             if weight > limit:
+                # heaviest person goes alone
                 ans += 1
                 right -= 1
             else:
+                # heaviest + lightest go together
                 ans += 1
                 left += 1
                 right -= 1
-        
+
         return ans
