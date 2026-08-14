6a922c0ec0a9df804fa0d9cd5aeeaddc5cdc488b
Ashwin <ashwin.mallik@mail.utoronto.ca>
2026-08-13 21:36:27 -0400

    Time: 72 ms (10.18%) | Memory: 35.2 MB (8.95%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -9,14 +9,14 @@ class Solution:
 
         left = 0
         right = k - 1
-        while left <= right:
+        while left < right:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
             right -= 1
         
         left = k
         right = len(nums) - 1
-        while left <= right:
+        while left < right:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
             right -= 1
